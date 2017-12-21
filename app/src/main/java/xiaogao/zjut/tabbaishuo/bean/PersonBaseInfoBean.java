4344bf34d867faacd7a5aa0475a4dc523ca3e1d0
laozhu <qqq2830@163.com>
2017-12-21 18:27:37 +0800

    添加了时间选择控件

---
+++ app/src/main/java/xiaogao/zjut/tabbaishuo/bean/PersonBaseInfoBean.java
@@ -0,0 +1,8 @@
+package xiaogao.zjut.tabbaishuo.bean;
+
+/**
+ * Created by Administrator on 2017/12/21.
+ */
+
+public class PersonBaseInfoBean {
+}
---
+++ app/src/main/java/xiaogao/zjut/tabbaishuo/interfaces/IUIActivitySetNickname.java
@@ -0,0 +1,8 @@
+package xiaogao.zjut.tabbaishuo.interfaces;
+
+/**
+ * Created by Administrator on 2017/12/21.
+ */
+
+public interface IUIActivitySetNickname {
+}
